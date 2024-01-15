95e38f9390b465656392e1cfde30c9f8d88b6de4
Sagor Mohammed Yousuf <smyousuf630@gmail.com>
2024-01-15 19:04:04 +0600

    update-code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     $(".create-modal").toggle();
   });
 
+
   // when click any where on my screen then close modal // 
   $(document).on('click', function (e) {
     if (!$(e.target).closest('.create-modal, #create-modal').length) {
@@ -63,41 +64,202 @@ $(document).ready(function () {
 
 
 
-  $(".customize-btn").click(function(e) {
-    e.stopPropagation(); // Prevent click from propagating to document
-    // $(".customize-modal").css("width" , "384px")
-    if ($(".customize-modal").css("right") === "-384px") {
-      // Show side-nav
-      $(".customize-modal").animate({ right: '0px' }, 100);
-      // $(".customize-modal").css("display" , "block")
-    } else {
-      // Hide side-nav
-      $(".customize-modal").animate({ right: '-384px' }, 100);
+  // $(".customize-btn").click(function(e) {
+  //   e.stopPropagation(); // Prevent click from propagating to document
+  //   // $(".customize-modal").css("width" , "384px")
+  //   if ($(".customize-modal").css("right") === "-384px") {
+  //     // Show side-nav
+  //     $(".customize-modal").animate({ right: '0px' }, 100);
+  //     // $(".customize-modal").css("display" , "block")
+  //   } else {
+  //     // Hide side-nav
+  //     $(".customize-modal").animate({ right: '-384px' }, 100);
       
-    }
-  });
+  //   }
+  // });
 
-  // Close the customize-modal when clicking anywhere on the screen
-  $(document).click(function(e) {
-    // Check if the clicked area is neither the customize-modal nor any of its descendants
-    if (!$(e.target).closest('.customize-modal').length) {
-      // Hide side-nav if it is open
-      if ($(".customize-modal").css("right") === "0px") {
-        $(".customize-modal").animate({ right: '-384px' }, 100);
-      }
-    }
-  });
+  // // Close the customize-modal when clicking anywhere on the screen
+  // $(document).click(function(e) {
+  //   // Check if the clicked area is neither the customize-modal nor any of its descendants
+  //   if (!$(e.target).closest('.customize-modal').length) {
+  //     // Hide side-nav if it is open
+  //     if ($(".customize-modal").css("right") === "0px") {
+  //       $(".customize-modal").animate({ right: '-384px' }, 100);
+  //     }
+  //   }
+  // });
 
-  // Prevent the click inside the customize-modal from propagating to document
-  $(".customize-modal").click(function(e) {
-    e.stopPropagation();
-  });
+  // // Prevent the click inside the customize-modal from propagating to document
+  // $(".customize-modal").click(function(e) {
+  //   e.stopPropagation();
+  // });
 
 
 
   $(".hide-customize-modal").click(function(){
     $(".customize-modal").css("right" , "-384px")
   })
+
+
+   // Initially hide the sidebar
+  $(".customize-modal").css("right", "-384px");
+
+  // Toggle sidebar on button click
+  $(".customize-btn").click(function(e) {
+    e.preventDefault();
+    e.stopPropagation(); // Prevent click from propagating
+    var currentRight = $(".customize-modal").css("right");
+    if (currentRight === "-384px") {
+      $(".customize-modal").animate({ right: '0px' }, 100);
+      $("body").addClass("modal-open"); // Add class to hide scrollbar
+    } else {
+      $(".customize-modal").animate({ right: '-384px' }, 100);
+      $("body").removeClass("modal-open"); // Remove class to show scrollbar
+    }
+  });
+
+  // Close the sidebar when clicking outside
+  $(document).click(function(e) {
+    if ($(".customize-modal").css("right") === "0px") {
+      $(".customize-modal").animate({ right: '-384px' }, 100);
+      $("body").removeClass("modal-open"); // Remove class to show scrollbar
+    }
+  });
+
+  // Prevent clicks inside the sidebar from propagating to the document
+  $(".customize-modal").click(function(e) {
+    e.stopPropagation();
+  });
+
+//  set bg  marron//
+$(".maroon").click(function () {
+ 
+   $(".home-page").css("background-image", "url('asset/Maroon_background_dark.jpg')");
+   $(".done-icon").css("display" , "none")
+   $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+//  set bg golden  //
+$(".golden").click(function () {
+  
+   $(".home-page").css("background-image", "url('asset/Orange_yellow_background_dark.jpg')");
+   
+   $(".done-icon").css("display" , "none")
+   $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+// set bg olive //
+
+$(".olive").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Yellow_green_background_dark.jpg')");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+// set bg deep-greenish-blue //
+$(".deep-greenish-blue").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Blue_green_background_dark.jpg')");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+// set bg blue-green 
+$(".deep-greenish-blue").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Blue_green_background_dark.jpg')");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+// set bg blue-green" 
+
+$(".blue-green").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Forest_background_dark.jpg')");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+
+// set bg pale-green" 
+
+$(".pale-green").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Aqua_background_dark.jpg')");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+// set bg steel-blue
+$(".steel-blue").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Blue_background_dark.jpg')");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+// set bg lavender-blue
+$(".lavender-blue").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Purple_background_dark.jpg");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+// set bg lavender-purple
+$(".lavender-purple").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Pink_purple_background_dark.jpg");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+// set bg salmon
+$(".salmon").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Pink_background_dark.jpg");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+// set bg pale-beige
+$(".pale-beige").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/Oat_background_dark.jpg");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
+
+
+// set bg dark-bg
+
+$(".dark-bg").click(function () {
+  
+  $(".home-page").css("background-image", "url('asset/classic_home_background_dark.png");
+  
+  $(".done-icon").css("display" , "none")
+  $(this).html("<img src='icon/done_.svg' class='done-icon'>");
+});
 
 
 
